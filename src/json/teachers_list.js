12ef3f4f77c7463cf36a00e755af1ce8d7112c9f
doritosxxx <georgyvav@ya.ru>
2020-09-20 21:21:53 +0300

    Update teachers_list.js
    для тестов

--- src/json/teachers_list.js
+++ src/json/teachers_list.js
@@ -34,4 +34,64 @@ export default [{
         }
     }
 
+}, {
+    "id": 4,
+    "image": "https://i1.sndcdn.com/artworks-000176843744-xziit4-t500x500.jpg",
+    "details": {
+        "name": "Scrolling test",
+        "rating": {
+            "kindness": 0,
+            "simplicity": 10,
+            "intelligibility": 10
+        }
+    }
+
+}, {
+    "id": 5,
+    "image": "https://i1.sndcdn.com/artworks-000176843744-xziit4-t500x500.jpg",
+    "details": {
+        "name": "one more",
+        "rating": {
+            "kindness": 0,
+            "simplicity": 10,
+            "intelligibility": 10
+        }
+    }
+
+}, {
+    "id": 6,
+    "image": "https://i1.sndcdn.com/artworks-000176843744-xziit4-t500x500.jpg",
+    "details": {
+        "name": "Search test",
+        "rating": {
+            "kindness": 0,
+            "simplicity": 10,
+            "intelligibility": 10
+        }
+    }
+
+}, {
+    "id": 7,
+    "image": "https://i1.sndcdn.com/artworks-000176843744-xziit4-t500x500.jpg",
+    "details": {
+        "name": "And also filters test",
+        "rating": {
+            "kindness": 0,
+            "simplicity": 10,
+            "intelligibility": 10
+        }
+    }
+
+}, {
+    "id": 3,
+    "image": "https://i1.sndcdn.com/artworks-000176843744-xziit4-t500x500.jpg",
+    "details": {
+        "name": "And also overflow test using long name",
+        "rating": {
+            "kindness": 0,
+            "simplicity": 10,
+            "intelligibility": 10
+        }
+    }
+
 }];
