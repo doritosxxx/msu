091e9aa878c42402792428aecdf3cfd9515dea53
doritosxxx <georgyvav@ya.ru>
2020-11-22 14:19:33 +0300

    Написал запросы к серверу

--- src/modules/Server/index.js
+++ src/modules/Server/index.js
@@ -1,7 +1,8 @@
 import Review from "../../classes/Review";
 import Teacher from "../../classes/Teacher"
 
-const APIUrl = "https://sta.profcomff.com/"
+const APIUrl = "https://sta.profcomff.com/api"
+const APIVersion = "v1"
 
 function HTTPBuildQuery(params) {
     if (typeof(params) != 'object')
@@ -12,29 +13,48 @@ function HTTPBuildQuery(params) {
 }
 
 async function request(method, params) {
-    // Настоятельно рекомендуется обработать исключения.
-    const response = await fetch(`${APIUrl}${method}${HTTPBuildQuery(params)}`)
+	// Настоятельно рекомендуется обработать исключения.
+	const url = `${APIUrl}/${APIVersion}/${method}${HTTPBuildQuery(params)}`
+    const response = await fetch(url)
     const rawObject = await response.json()
-
     return rawObject;
 }
 
 class Server {
     static async GetTeacherById(id) {
-        const teacher = await request('teacher.get', { id })
-        return new Teacher(teacher);
+		try {
+			const teacher = await request(`teacher/${id}`)
+			return new Teacher(teacher);
+		}
+		catch {
+			return new Teacher({});
+		}
 	}
 	
-	static async GetTeacherRange(offset, count = 20){
-		const teachersList = await request('teacher.get_all', {
-			offset: offset,
-			top: count
-		})
-		return teachersList.map(teacher => new Teacher(teacher))
+	static async GetTeachersRange(offset, count = 20, orderBy = 'score_general'){
+		try {
+			const teachersList = await request('teacher', {
+				offset: offset,
+				limit: count,
+				orderby: orderBy
+			})
+			return teachersList.map(teacher => new Teacher(teacher));
+		}
+		catch {
+			return [];
+		}
 	}
 
-	static async GetReview(){
-		return new Review({});
+	static async GetReviewsByTeacherId(teacherId){
+		try {
+			const reviewsList = await request(`teacher/${teacherId}/comment`, {
+				teacher_id : teacherId
+			})
+			return reviewsList.map(review => new Review(review));
+		}
+		catch {
+			return [];
+		}
 	}
 
 }
